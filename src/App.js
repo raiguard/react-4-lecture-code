41e93b69f3b8e6bf1db0a7c1238df58160e356a7
mattcbodily <mattcbodily@gmail.com>
2020-06-22 09:45:34 -0600

    updated boiler-plate

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ class App extends Component {
     return (
       <div className='App'>
         <img className='pokemon-logo' src='https://1000logos.net/wp-content/uploads/2017/05/Pokemon-Logo.png' alt='Pokemon'/>
-        <br/>
-        <img className='pokedex' src='https://bonnichiwa.github.io/interactive-game-app/images/pokedex2.png' alt='pokedex'/>
       </div>
     )
   }
